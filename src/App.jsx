7ac5c743ef08e120edf9c07318cbfb4daece6be6
Kayhlacr <kayhla@student.dev-island.org>
2024-02-22 19:50:43 -1000

    create ContactList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import "./App.css";
-import ContactCard from "./components/ContactCard/ContactCard";
+import ContactList from "./components/ContactList/ContactList";
+import dummyContacts from "./contacts";
 
 function App() {
+  const [contacts, setContacts] = useState(dummyContacts);
+
   return (
     <>
       <h1>React Contacts</h1>
-      <ContactCard
-        name={"Jenny Smith"}
-        phone={"808 - 867 - 5309"}
-        email={"jenny@gmail.com"}
-      />
+      <ContactList contacts={contacts} />
     </>
   );
 }
